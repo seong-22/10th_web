643f5e4f01ada92a69e48c07986e2a4cb7074119
오성이 <hospi0539@naver.com>
2026-03-17 19:48:18 +0900

    feat: week00 mission 파일 추가

---
+++ week01/keyword/key.ts
@@ -0,0 +1,21 @@
+const str2 : string = "aung"; //성공
+
+const num2 : number = 456; //성공
+
+const bool2 : boolean = true; //성공
+
+const Null : null = null; //성공
+
+const Undefined : undefined = undefined; //성공
+
+const Symbol2 : symbol = Symbol("aung"); //성공
+
+const BigInt2 : bigint = 1234567890123456789012345678901234567890n; //성공
+
+const OOB : object = { name: "aung", age: 30 }; //성공
+
+const matthew = { name: 'matthew'}; //객체리터럴 -> string으로 취급
+matthew.name = 'hi'; 
+
+const name2 : "netflix" = "netflix"; //문자리터럴 -> 수정 불가
+name2 = "tving";
